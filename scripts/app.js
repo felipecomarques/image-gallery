46de6ee38d7c66f812e6e88d1322eb6a000e4043
Felipe Marques <felipe.comarques1208@gmail.com>
2023-12-05 19:35:23 -0300

    feat: Add functionality to navigate the gallery

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+const divImagemPrincipal = document.querySelector('#imagem-principal');
+const imagemPrincipal = divImagemPrincipal.querySelector('img');
+const textoAlternativo = divImagemPrincipal.querySelector('.texto-alternativo');
+const btnProximo = divImagemPrincipal.querySelector('.proximo');
+const btnAnterior = divImagemPrincipal.querySelector('.anterior');
+const todasImagens = document.querySelectorAll('#imagens img');
+
+let idImagemAtiva = 0;
+
+const proximaImagem = function() {
+    idImagemAtiva++;
+    if(idImagemAtiva >= todasImagens.length)
+        idImagemAtiva = 0;
+    selecionarImagem();
+}
+
+const voltarImagem = function() {
+    idImagemAtiva--;
+    if(idImagemAtiva < 0)
+        idImagemAtiva = todasImagens.length - 1;
+    selecionarImagem();
+}
+
+const selecionarImagem = function(){
+    imagemPrincipal.src = todasImagens[idImagemAtiva].src;
+    todasImagens.forEach(function(imagem){
+        imagem.classList = "";
+    });
+    todasImagens[idImagemAtiva].classList.add("");
+}
+
+btnProximo.addEventListener("click", proximaImagem);
+btnAnterior.addEventListener("click", voltarImagem);
+
+todasImagens.forEach(function (imagem, numeroImage){
+    imagem.addEventListener("click", function(){
+        // ainda precisa fazer
+    });
+});
